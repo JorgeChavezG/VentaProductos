1f5a3984a5ddcae40ca695aee84fd45cff8b71af
JorgeChavezG <jorge10009@hotmail.com>
2022-11-07 00:50:26 -0500

    Agregar archivos de proyecto.

---
+++ VentaProductos/Controllers/AccesoController.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VentaProductos.Models;
+using System.Data.SqlClient;
+using System.Data;
+using System.Web.Services.Description;
+
+namespace VentaProductos.Controllers
+{
+
+    
+    public class AccesoController : Controller
+    {
+            static string cadena = "Data Source=LAPTOP-8SARE4LH\\MSSQLSERVER02;Initial Catalog=Venta_Productos;Integrated Security =true";
+
+        // GET: Acceso
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult Registrar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Registrar(Usuario oUsuario)
+        {
+            bool registrado;
+            string mensaje;
+
+            if (oUsuario.Clave == oUsuario.ConfirmarClave)
+            {
+                oUsuario.Clave = oUsuario.Clave;
+            }
+            else
+            {
+                ViewData["Mensaje"] = "Contraseña no coinciden";
+                return View();
+            }
+
+            using (SqlConnection cn = new SqlConnection(cadena))
+            {
+
+                SqlCommand cmd = new SqlCommand("usp_insertar_usuario", cn);
+                cmd.Parameters.AddWithValue("Correo", oUsuario.Correo);
+                cmd.Parameters.AddWithValue("Clave", oUsuario.Clave);
+                cmd.Parameters.Add("Registro", SqlDbType.Bit).Direction = ParameterDirection.Output;
+                cmd.Parameters.Add("Mensaje", SqlDbType.VarChar,50).Direction = ParameterDirection.Output;
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cn.Open();
+
+                cmd.ExecuteNonQuery();
+
+                registrado = Convert.ToBoolean(cmd.Parameters["Registro"].Value);
+                mensaje = cmd.Parameters["Mensaje"].Value.ToString();
+            }
+
+            ViewData["Mensaje"] = mensaje;
+
+            if (registrado)
+            {
+                return RedirectToAction("Login", "Acceso");               
+            }
+            else
+            {
+                return View();
+            }
+            
+            
+        }
+        [HttpPost]
+        public ActionResult Login(Usuario oUsuario)
+        {
+            oUsuario.Clave = oUsuario.Clave;
+
+        using (SqlConnection cn = new SqlConnection(cadena))
+            {
+                SqlCommand cmd = new SqlCommand("usp_validar_usuario", cn);
+                cmd.Parameters.AddWithValue("Correo", oUsuario.Correo);
+                cmd.Parameters.AddWithValue("Clave", oUsuario.Clave);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cn.Open();
+
+               oUsuario.idUsuario = Convert.ToInt32( cmd.ExecuteScalar().ToString());
+
+            }
+        if (oUsuario.idUsuario != 0)
+            {
+                Session["usuario"] = oUsuario;
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ViewData["Mensaje"] = "Usuario no encontrado";
+
+                return View();
+            }
+            
+            
+        }
+
+
+    }
+}
